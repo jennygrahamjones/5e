f84cb6d65a1d1f4fc8a7dae9b5a87f972b2d2178
Jenny Graham-Jones <jenny.grahamjones@gmail.com>
2024-09-12 20:33:26 -0400

    Publish action

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    basePath: '/5e'
+  };
+  
+  export default nextConfig;
+  
